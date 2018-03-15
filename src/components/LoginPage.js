516d04df9af19b5b6a75be1bced5cfc742afc8be
Frank Murphy <frank.nds@gmail.com>
2018-03-15 10:50:35 +0000

    Added Styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Log in with Google</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensify</h1>
+            <p>This was developed as part of the Udemy course "The Complete React Web Development course (with Redux)". To use it, logon with your google id using the button below.</p>
+            <button className="button" onClick={startLogin}>Log in with Google</button>
+        </div>
     </div>
 );
 
